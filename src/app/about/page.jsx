0794a25288d72294d029a6328d7f12b11724d9ac
Pavel <pavel153das@gmail.com>
2025-04-19 11:32:05 +0530

    Update page.jsx

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <div>
+      <div>
+        <h1>About</h1>
+        <p>This is the about page of our application.</p>
+      </div>
+      <div>
+        <h2>My Skills</h2>
+      </div>
+      <div>
+        <h2>My Projects</h2>
+        <ul>
+          <li>Project 1</li>
+          <li>Project 2</li>
+          <li>Project 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default About;
